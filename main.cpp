95e9c9ca1a10431fee48d56197fa4e5da4378f39
Quinnboi10 <quinnboi10@noreply.nocturn9x.space>
2024-12-27 00:26:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -34,6 +34,7 @@
 #include <memory>
 #include <random>
 #include <cstring>
+#include <immintrin.h>
 
 #define ctzll(x) std::countr_zero(x)
 #define popcountll(x) std::popcount(x)
@@ -2091,17 +2092,45 @@ int NNUE::forwardPass(Board* board) {
     // Accumulate output for STM and OPP using separate weight segments
     i64 eval = 0;
 
-    for (int i = 0; i < HL_SIZE; i++) {
-        // First HL_SIZE weights are for STM
-        if constexpr (activation == ::ReLU) eval += ReLU(accumulatorSTM[i]) * weightsToOut[i];
-        if constexpr (activation == ::CReLU) eval += CReLU(accumulatorSTM[i]) * weightsToOut[i];
-        if constexpr (activation == ::SCReLU) eval += SCReLU(accumulatorSTM[i]) * weightsToOut[i];
+    if constexpr (activation != ::SCReLU) {
+        for (int i = 0; i < HL_SIZE; i++) {
+            // First HL_SIZE weights are for STM
+            if constexpr (activation == ::ReLU) eval += ReLU(accumulatorSTM[i]) * weightsToOut[i];
+            if constexpr (activation == ::CReLU) eval += CReLU(accumulatorSTM[i]) * weightsToOut[i];
 
-        // Last HL_SIZE weights are for OPP
-        if constexpr (activation == ::ReLU) eval += ReLU(accumulatorOPP[i]) * weightsToOut[HL_SIZE + i];
-        if constexpr (activation == ::CReLU) eval += CReLU(accumulatorOPP[i]) * weightsToOut[HL_SIZE + i];
-        if constexpr (activation == ::SCReLU) eval += SCReLU(accumulatorOPP[i]) * weightsToOut[HL_SIZE + i];
+            // Last HL_SIZE weights are for OPP
+            if constexpr (activation == ::ReLU) eval += ReLU(accumulatorOPP[i]) * weightsToOut[HL_SIZE + i];
+            if constexpr (activation == ::CReLU) eval += CReLU(accumulatorOPP[i]) * weightsToOut[HL_SIZE + i];
+        }
     }
+    else {
+        const __m256i vec_zero = _mm256_setzero_si256();
+        const __m256i vec_qa = _mm256_set1_epi16(inputQuantizationValue);
+        __m256i sum = vec_zero;
+
+        for (int i = 0; i < HL_SIZE / 16; i++) {
+            const __m256i us = _mm256_load_si256(reinterpret_cast<const __m256i*>(&accumulatorSTM[16 * i])); // Load from accumulator
+            const __m256i them = _mm256_load_si256(reinterpret_cast<const __m256i*>(&accumulatorOPP[16 * i]));
+
+            const __m256i us_weights = _mm256_load_si256(reinterpret_cast<const __m256i*>(&weightsToOut[16 * i])); // Load from net
+            const __m256i them_weights = _mm256_load_si256(reinterpret_cast<const __m256i*>(&weightsToOut[HL_SIZE + 16 * i]));
+
+            const __m256i us_clamped = _mm256_min_epi16(_mm256_max_epi16(us, vec_zero), vec_qa);
+            const __m256i them_clamped = _mm256_min_epi16(_mm256_max_epi16(them, vec_zero), vec_qa);
+
+            const __m256i us_results = _mm256_madd_epi16(_mm256_mullo_epi16(us_weights, us_clamped), us_clamped);
+            const __m256i them_results = _mm256_madd_epi16(_mm256_mullo_epi16(them_weights, them_clamped), them_clamped);
+
+            sum = _mm256_add_epi32(sum, us_results);
+            sum = _mm256_add_epi32(sum, them_results);
+        }
+
+        __m256i v1 = _mm256_hadd_epi32(sum, sum);
+        __m256i v2 = _mm256_hadd_epi32(v1, v1);
+        
+        eval = _mm256_extract_epi32(v2, 0) + _mm256_extract_epi32(v2, 4);
+    }
+    
 
     // Dequantization
     if constexpr (activation == ::SCReLU) eval /= inputQuantizationValue;
