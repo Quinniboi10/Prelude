6b218f14afedcb5c468e084e8f31150b5a65234e
Quinniboi10 <quinniboi10@noreply.nocturn9x.space>
2025-01-29 04:29:45 +0100

    Cleanup config
    Bench: 7511502

--- src/config.h
+++ src/config.h
@@ -2,16 +2,17 @@
 
 #include "types.h"
 
-// ****** SEARCH ******
-constexpr int RFP_MARGIN            = 75;
-constexpr int NMP_REDUCTION         = 3;  // NMP depth reduction
-constexpr int NMP_REDUCTION_DIVISOR = 3;  // Subtract depth/n from NMP depth
-
+// ************ SEARCH ************
 constexpr int MATE_SCORE = 32767;  // Max for i16
 constexpr int MAX_PLY    = 255;
 
 constexpr int MATE_IN_MAX_PLY  = MATE_SCORE - MAX_PLY;
 constexpr int MATED_IN_MAX_PLY = -MATE_SCORE + MAX_PLY;
+
+// ****** PRUNING ******
+constexpr int RFP_MARGIN            = 75;
+constexpr int NMP_REDUCTION         = 3;  // NMP depth reduction
+constexpr int NMP_REDUCTION_DIVISOR = 3;  // Subtract depth/n from NMP depth
 
 constexpr int MIN_MOVES_BEFORE_LMP = 7;  // Only does LMP if moves search >= n * depth * depth
 constexpr int MAX_DEPTH_FOR_LMP    = 5;  // Only does late move pruning if depth <= n
@@ -24,16 +25,18 @@ constexpr int PVS_SEE_CAPTURE_SCALAR = -30;
 
 constexpr int SEE_MARGIN = -108;  // Margin for qsearch SEE pruning
 
+// ****** TIME MANAGEMENT ******
 constexpr int    DEFAULT_MOVES_TO_GO = 20;    // Defualt moves remaining in the game for time management
 constexpr int    INC_SCALAR          = 1;     // Amount of increment to add to hard time limit
 constexpr double SOFT_TIME_SCALAR    = 0.65;  // Scales the soft limit as hardLimit * n
 
+// ****** OTHER ******
 constexpr int    ASPR_DELTA           = 25;    // Used as delta size in aspiration window
 constexpr double ASP_DELTA_MULTIPLIER = 1.25;  // Scalar to widen aspr window on fail
 
 constexpr int MAX_HISTORY = 16384;  // Max history bonus
 
-// ****** DATA GEN ******
+// ************ DATA GEN ************
 constexpr int TARGET_POSITIONS   = 1'000'000'000;  // Number of positions to generate
 constexpr int OUTPUT_BUFFER_SIZE = 1;              // Size (MiB) for the game writing output buffer
 constexpr int RAND_MOVES         = 8;              // Number of random halfmoves before data gen begins
@@ -41,7 +44,7 @@ constexpr int NODES_PER_MOVE     = 5000;           // Soft nodes per move
 constexpr int MAX_NODES_PER_MOVE = 100'000;        // Hard nodes per move
 
 
-// ****** NNUE ******
+// ************ NNUE ************
 constexpr i16    QA             = 255;
 constexpr i16    QB             = 64;
 constexpr i16    EVAL_SCALE     = 400;
