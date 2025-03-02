b3db8fed90f90b16093e9207e3d5877cb9daad0f
Quinniboi10 <jsmithwek@outlook.com>
2025-03-01 18:00:45 -0600

    Change NMP params Bench: 6902326

--- src/search.cpp
+++ src/search.cpp
@@ -103,7 +103,7 @@ i32 search(Board& board, i32 depth, i32 ply, int alpha, int beta, Stack* ss, Thr
             return staticEval;
 
         // Null move pruning
-        if (board.canNullMove()) {
+        if (board.canNullMove() && staticEval >= beta) {
             Board testBoard = board;
             testBoard.nullMove();
             i32 eval = -search<NodeType::NONPV>(testBoard, depth - NMP_REDUCTION, ply + 1, -beta, -beta + 1, ss + 1, thisThread, sl);
