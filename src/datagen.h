ce8eaffbc7bbc5a53f565de86b4479d231902d2f
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2025-04-14 14:51:06 -0600

    Datagen tweak (nonfunctional) Bench: 4883709

--- src/datagen.h
+++ src/datagen.h
@@ -3,7 +3,7 @@
 #include "types.h"
 
 namespace Datagen {
-constexpr usize OUTPUT_BUFFER_GAMES = 1000;
+constexpr usize OUTPUT_BUFFER_GAMES = 50;
 constexpr usize RAND_MOVES          = 8;
 constexpr u64   SOFT_NODES          = 5000;
 constexpr u64   HARD_NODES          = 100'000;
