499326c13673d46498bfc848c53176d9d2fd14b8
Quinniboi10 <69563433+Quinniboi10@users.noreply.github.com>
2025-10-09 17:45:14 -0500

    Don't dive into QS when in check Bench: 15339478

--- src/search.cpp
+++ src/search.cpp
@@ -102,8 +102,11 @@ i32 search(Board& board, i32 depth, usize ply, int alpha, int beta, SearchStack*
         thisThread.seldepth = ply;
     if (board.isDraw() && ply > 0)
         return 0;
-    if (depth <= 0)
+    if (depth <= 0 && !board.inCheck())
         return qsearch<isPV>(board, ply, alpha, beta, ss, thisThread);
+
+    if (depth < 0)
+        depth = 0;
 
     // Mate distance pruning
     if (ply > 0) {
